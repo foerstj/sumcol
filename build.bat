a429c58dbaa6cda40121642b01f8793589a5c922
Johannes Förstner <johannes@famfoerstner.de>
2023-07-31 16:14:26 +0200

    Rename output dsres file

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: This script is supposed to be executed from your DS installation folder.
-:: TankCreator and gaspy are expected to be in sibling dirs.
+:: TankCreator is expected to be in a sibling dir.
 
 :: name of map
 set map=sumcol-demo
@@ -7,14 +7,14 @@ set map=sumcol-demo
 set map_cs=Summons Collection Demo
 :: name of resources
 set res=sumcol
+:: name of resources, case-sensitive
+set res_cs=Summons Collection
 :: path of DSLOA documents dir (where Bits are)
 set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
 :: path of DS installation
 set ds=.
 :: path of TankCreator
 set tc=..\TankCreator
-:: path of GasPy
-set gaspy=..\gaspy
 
 
 :: Compile map file
@@ -27,7 +27,7 @@ if %errorlevel% neq 0 pause
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art\bitmaps\gui" "%tmp%\Bits\art\bitmaps\gui" /E /xf *.psd
 robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" /E /xf .gitignore
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%res_cs%.dsres" -copyright "CC-BY-SA 2023" -title "%res_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
--- cleanup.bat
+++ cleanup.bat
@@ -1,9 +1,9 @@
 :: This script is supposed to be executed from your DS installation folder.
 
-:: name of map, case-sensitive
-set map_cs=Summons Collection Demo
+:: name of resources, case-sensitive
+set res_cs=Summons Collection
 :: path of DS installation
 set ds=.
 
 :: Cleanup resources so as not to confuse Siege Editor
-del "%ds%\DSLOA\%map_cs%.dsres"
+del "%ds%\DSLOA\%res_cs%.dsres"
