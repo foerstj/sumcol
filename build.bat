7b00feb9d88e32d51d1bf47c55f23f50ea884f3f
Johannes Förstner <johannes@famfoerstner.de>
2023-08-16 21:06:41 +0200

    Separate dsres for demo map resources

--- build.bat
+++ build.bat
@@ -23,12 +23,18 @@ robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E /x
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
-:: Compile resource file
+:: Compile mod resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art\bitmaps\gui" "%tmp%\Bits\art\bitmaps\gui" /E /xf *.psd
 robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" /E /xf .gitignore
 robocopy "%doc_dsloa%\Bits\world\global\effects" "%tmp%\Bits\world\global\effects" %res%-* /S
 %tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%res_cs%.dsres" -copyright "CC-BY-SA 2023" -title "%res_cs%" -author "Johannes Förstner"
+if %errorlevel% neq 0 pause
+
+:: Compile demo resource file
+rmdir /S /Q "%tmp%\Bits"
+robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%res%-demo" "%tmp%\Bits\world\contentdb\templates\%res%-demo" /E /xf .gitignore
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
--- cleanup.bat
+++ cleanup.bat
@@ -1,5 +1,7 @@
 :: This script is supposed to be executed from your DS installation folder.
 
+:: name of map, case-sensitive
+set map_cs=Summons Collection Demo
 :: name of resources, case-sensitive
 set res_cs=Summons Collection
 :: path of DS installation
@@ -7,3 +9,4 @@ set ds=.
 
 :: Cleanup resources so as not to confuse Siege Editor
 del "%ds%\DSLOA\%res_cs%.dsres"
+del "%ds%\DSLOA\%map_cs%.dsres"
