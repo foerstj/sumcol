f993971c5406aeb542e0e9b4fa2e649d738047f2
Johannes Förstner <johannes@famfoerstner.de>
2023-12-09 12:42:51 +0100

    Include in build

--- build.bat
+++ build.bat
@@ -34,6 +34,7 @@ if %errorlevel% neq 0 pause
 :: Compile all-in-one resource file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\art\bitmaps\gui" "%tmp%\Bits\art\bitmaps\gui" /xf *.psd /S
+robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" %res%.*.gas /S
 robocopy "%doc_dsloa%\Bits\world\ai\jobs\%res%" "%tmp%\Bits\world\ai\jobs\%res%" /S
 robocopy "%doc_dsloa%\Bits\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /S
 robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" /S
@@ -83,6 +84,7 @@ exit /b %errorlevel%
   echo build_partial %infix% %name%
   rmdir /S /Q "%tmp%\Bits"
   robocopy "%doc_dsloa%\Bits\art\bitmaps\gui" "%tmp%\Bits\art\bitmaps\gui" *-%infix%-* /xf *.psd /S
+  robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" %res%.*.gas /S
   robocopy "%doc_dsloa%\Bits\world\ai\jobs\%res%" "%tmp%\Bits\world\ai\jobs\%res%" /S
   robocopy "%doc_dsloa%\Bits\world\contentdb\components" "%tmp%\Bits\world\contentdb\components" /S
   robocopy "%doc_dsloa%\Bits\world\contentdb\templates\%res%" "%tmp%\Bits\world\contentdb\templates\%res%" common-* *-%infix%-* /S
