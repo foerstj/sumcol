d094976bccb27575d7f441442925573e26651d9a
Johannes Förstner <johannes@famfoerstner.de>
2024-06-15 18:24:32 +0200

    Move spells to subdir

--- generate.bat
+++ generate.bat
@@ -1,4 +1,4 @@
 pushd "%GasPy%"
-venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\interactive world\contentdb\templates\sumcol\interactive
+venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\interactive world\contentdb\templates\sumcol\interactive\spells
 venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo world\contentdb\templates\sumcol-demo
 popd
