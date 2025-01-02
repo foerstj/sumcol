5564ad8b4b080e7119e7f3afff1f90feb2bad11f
Johannes Förstner <johannes@famfoerstner.de>
2025-01-02 15:40:38 +0100

    Generate sub-summon spells with enchantments

--- generate.bat
+++ generate.bat
@@ -2,5 +2,9 @@ set stable=%1
 
 pushd "%GasPy%"
 venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\interactive world\contentdb\templates\sumcol\interactive\spells\summon --value stable=%stable%
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable%
+if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo world\contentdb\templates\sumcol-demo --value stable=%stable%
+if %errorlevel% neq 0 pause
 popd
