e3d93b06caf2668f3a63d963fb46aa9524a2318f
Johannes Förstner <johannes@famfoerstner.de>
2025-02-03 17:24:55 +0100

    Add test companions generation back in, oops

--- generate.bat
+++ generate.bat
@@ -7,4 +7,6 @@ venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\sub world\co
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable%
 if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable%
+if %errorlevel% neq 0 pause
 popd
