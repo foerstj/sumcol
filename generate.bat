92c8e57893ef0d3dd12b642798b2609590b5e19c
Johannes Förstner <johannes@famfoerstner.de>
2025-02-03 18:24:18 +0100

    Move jinja templates & csvs into /gaspy folder

--- generate.bat
+++ generate.bat
@@ -1,12 +1,12 @@
 set stable=%1
 
 pushd "%GasPy%"
-venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\main world\contentdb\templates\sumcol\interactive\spells\summon --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\main world\contentdb\templates\sumcol\interactive\spells\summon --value stable=%stable%
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable%
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable%
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja world\contentdb\templates.jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable%
 if %errorlevel% neq 0 pause
 popd
