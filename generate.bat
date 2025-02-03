665b8ed107e8a2b477b04ea7760825971a790683
Johannes Förstner <johannes@famfoerstner.de>
2025-02-03 21:26:14 +0100

    Auto-generate from jinja & csv! Ironing out some specialties to keep things simple

--- generate.bat
+++ generate.bat
@@ -5,6 +5,10 @@ venv\Scripts\python -m jinja gaspy\jinja\sumcol\main world\contentdb\templates\s
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja gaspy\jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable%
 if %errorlevel% neq 0 pause
+
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-loa-nstd.de.gas.csv --value v=loa --value ft=nstd
+if %errorlevel% neq 0 pause
+
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable%
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable%
