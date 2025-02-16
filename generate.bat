eef655c5a381ebbd91f0421f719df84ba116d978
Johannes Förstner <johannes@famfoerstner.de>
2025-02-16 09:30:51 +0100

    Make generate.bat location independent

--- generate.bat
+++ generate.bat
@@ -1,22 +1,25 @@
 set stable=%1
 
+:: path of Bits dir
+set bits=%~dp0.
+
 pushd "%GasPy%"
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\main world\contentdb\templates\sumcol\interactive\spells\summon --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\main world\contentdb\templates\sumcol\interactive\spells\summon --value stable=%stable% --bits "%bits%"
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable%
-if %errorlevel% neq 0 pause
-
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-loa-nstd.de.gas.csv --value v=loa --value ft=nstd
-if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-loa-std.de.gas.csv --value v=loa --value ft=std
-if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-v-nstd.de.gas.csv --value v=v --value ft=nstd
-if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-v-std.de.gas.csv --value v=v --value ft=std
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\sub world\contentdb\templates\sumcol\interactive\spells\monster --value stable=%stable% --bits "%bits%"
 if %errorlevel% neq 0 pause
 
-venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-loa-nstd.de.gas.csv --value v=loa --value ft=nstd --bits "%bits%"
 if %errorlevel% neq 0 pause
-venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable%
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-loa-std.de.gas.csv --value v=loa --value ft=std --bits "%bits%"
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-v-nstd.de.gas.csv --value v=v --value ft=nstd --bits "%bits%"
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja gaspy\jinja\sumcol\language language --for-all gaspy\jinja\sumcol\language\sumcol-v-std.de.gas.csv --value v=v --value ft=std --bits "%bits%"
+if %errorlevel% neq 0 pause
+
+venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable% --bits "%bits%"
+if %errorlevel% neq 0 pause
+venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map world\contentdb\templates\sumcol-demo --value stable=%stable% --bits "%bits%"
 if %errorlevel% neq 0 pause
 popd
