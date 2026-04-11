0df14f1a62fc2c54b18c430cc638c0e4d9c08df5
Johannes Förstner <johannes@famfoerstner.de>
2026-04-11 11:32:04 +0200

    Minor move-around & newlines in generate.bat

--- generate.bat
+++ generate.bat
@@ -5,17 +5,12 @@ set vanilla=%2
 set bits=%~dp0.
 
 pushd "%GasPy%"
+
 :: SumCol main
 venv\Scripts\python -m jinja gaspy\jinja\sumcol\main world\contentdb\templates\sumcol\interactive\spells\summon --for-each gaspy\jinja\sumcol\main\main.csv --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja gaspy\jinja\sumcol\monster-spells world\contentdb\templates\sumcol\interactive\spells\monster --for-each gaspy\jinja\sumcol\monster-spells\main.csv --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
 if %errorlevel% neq 0 pause
-setlocal enableDelayedExpansion
-for %%x in (originals) do (
-  venv\Scripts\python -m jinja gaspy\jinja\sumcol\monster-spells world\contentdb\templates\sumcol\x\%%x\interactive\spells\monster "spell-x-{{x}}-{{dmgtype}}-{{name}}-{{mc}}-{{ct}}-{{ft}}-{{v}}-{{stn}}.gas" --for-each gaspy\jinja\sumcol\monster-spells\x-%%x.csv --value x=%%x --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
-  if !errorlevel! neq 0 pause
-)
-endlocal
 venv\Scripts\python -m jinja gaspy\jinja\sumcol\monster-weapons world\contentdb\templates\sumcol\interactive\weapons --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja gaspy\jinja\sumcol\monster-actors world\contentdb\templates\sumcol\actors\evil --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
@@ -48,6 +43,13 @@ for %%x in (guards originals) do (
   if !errorlevel! neq 0 pause
 )
 endlocal
+setlocal enableDelayedExpansion
+for %%x in (originals) do (
+  venv\Scripts\python -m jinja gaspy\jinja\sumcol\monster-spells world\contentdb\templates\sumcol\x\%%x\interactive\spells\monster "spell-x-{{x}}-{{dmgtype}}-{{name}}-{{mc}}-{{ct}}-{{ft}}-{{v}}-{{stn}}.gas" --for-each gaspy\jinja\sumcol\monster-spells\x-%%x.csv --value x=%%x --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
+  if !errorlevel! neq 0 pause
+)
+endlocal
+
 
 :: SumCol Mart map
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-mart\mart-content.gas.jinja world\contentdb\templates\sumcol-mart --for-all gaspy\jinja\sumcol\main\main.csv --bits "%bits%"
@@ -60,6 +62,7 @@ for %%x in (guards originals) do (
 endlocal
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-mart\mart-content-originals.gas.jinja world\contentdb\templates\sumcol-mart --for-all gaspy\jinja\sumcol-mart\originals.csv --bits "%bits%"
 if %errorlevel% neq 0 pause
+
 
 :: SumCol demo/test map
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\summons world\contentdb\templates\sumcol-demo\summons\interactive\spells\summon --value stable=%stable% --value vanilla=%vanilla% --bits "%bits%"
@@ -88,4 +91,5 @@ venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map\containers\test-only.ga
 if %errorlevel% neq 0 pause
 venv\Scripts\python -m jinja gaspy\jinja\sumcol-demo\map\containers\test-only-x.gas.jinja world\contentdb\templates\sumcol-demo\containers --for-all gaspy\jinja\sumcol\main\x-guards.csv gaspy\jinja\sumcol\main\x-originals.csv --value stable=%stable% --bits "%bits%"
 if %errorlevel% neq 0 pause
+
 popd
